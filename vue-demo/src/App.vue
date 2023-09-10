909c552dd2b5b9e35760ef47876fee75fa8824ff
lgb <lgb@gmail.com>
2023-09-10 21:10:05 +0800

    11

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <!--第三步.使用组件-->
+  <CompositionImport2 />
+
   <CompositionImport />
   <TheWelcome />
   <IFVue />
@@ -17,4 +20,18 @@ import FromData from './components/from.vue'
 import TemplateImport from './components/Template.vue'
 import CompositionImport from './components/Composition.vue'
 
+//第一步.引入组件
+import CompositionImport2 from './components/Compostition2.vue'
+
+//如果<script>标签中有setup()函数(<script setup>)，那么就不需要再写export default了,有setup为组合式 API 
+/*export default {
+  //第二步.注入组件
+  components: {
+    CompositionImport2,
+    CompositionImport
+  }
+}*/
+
 </script>
+
+<style></style>
---
+++ vue-demo/src/components/Compostition2.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="base_comp">组件基础</div>
+</template>
+  
+<script setup>
+
+</script>
+  
+<style scoped>
+.base_comp {
+    font-size: 30px;
+    color: red;
+}
+</style>
